39ed43f8f1d027d511e82dc2c820bd5eb7ba50c2
OsugiTakuya <t.4.103.tkynt@gmail.com>
2018-10-26 10:07:41 +0900

    Create git_practice.php

---
+++ git_practice.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!-- http://localhost/practice/git_practice.php -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Gitの練習</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      echo "gitの練習をします";
+      ?>
+    </p>
+  </body>
+</html>
